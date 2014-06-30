bbdaba2b78f5aa4089108d69531f70486048edf0
Almer Almazan <almeralmazan@gmail.com>
2014-06-30 18:07:06 +0800

    Add 3 methods for viewing of pages in HomeController class

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,10 +2,27 @@
 
 class HomeController extends BaseController {
 
-	public function index()
+	public function home()
 	{
-        $title = 'Home Page';
+        $title = 'Home';
 		return View::make('home.index', compact('title'));
 	}
 
+    public function applicationForm()
+    {
+        $title = 'Application Form';
+        return View::make('home.application_form', compact('title'));
+    }
+
+    public function listOfPassers()
+    {
+        $title = 'List Of Passers';
+        return View::make('home.list_of_passers', compact('title'));
+    }
+
+    public function payment()
+    {
+        $title = 'Payment';
+        return View::make('home.payment', compact('title'));
+    }
 }
