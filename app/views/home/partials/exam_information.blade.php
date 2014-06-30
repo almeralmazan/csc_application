fd320a15a1731f30f15e147dd755bf89da09d490
Almer Almazan <almeralmazan@gmail.com>
2014-07-01 07:44:36 +0800

    Remove button clear for date input fields

--- app/views/home/partials/exam_information.blade.php
+++ app/views/home/partials/exam_information.blade.php
@@ -70,9 +70,8 @@
             </div>
             <div class="col-md-3">
                 <!-- <input type="text" class="form-control" id="gender" placeholder="Suffix"> -->
-                <div class="input-group date form_date col-md-12" data-date="" data-date-format="dd MM yyyy" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">
+                <div class="input-group date form_date col-md-12" data-date="" data-date-format="MM d, yyyy" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">
                     <input class="form-control" size="16" type="text" value="" readonly>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
                     <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
                 </div>
                 <input type="hidden" id="dtp_input2" value="" />
@@ -84,9 +83,8 @@
             </div>
             <div class="col-md-3">
                 <!-- <input type="text" class="form-control" id="gender" placeholder="Suffix"> -->
-                <div class="input-group date form_date col-md-12" data-date="" data-date-format="dd MM yyyy" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">
+                <div class="input-group date form_date col-md-12" data-date="" data-date-format="MM d, yyyy" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">
                     <input class="form-control" size="16" type="text" value="" readonly>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
                     <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
                 </div>
                 <input type="hidden" id="dtp_input2" value="" />
@@ -156,9 +154,8 @@
             </div>
             <div class="col-md-3">
                 <!-- <input type="text" class="form-control" id="gender" placeholder="Suffix"> -->
-                <div class="input-group date form_date col-md-12" data-date="" data-date-format="dd MM yyyy" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">
+                <div class="input-group date form_date col-md-12" data-date="" data-date-format="MM d, yyyy" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">
                     <input class="form-control" size="16" type="text" value="" readonly>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
                     <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
                 </div>
                 <input type="hidden" id="dtp_input2" value="" />
