216f9a19f2585ac0f7adc551a0a2a38f0e768b2c
Almer Almazan <almeralmazan@gmail.com>
2014-06-30 20:45:02 +0800

    Add home content to index.blade.php

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Home</h1>
+<div class="jumbotron">
+    <div class="row">
+        <div class="col-md-12">
+            {{ HTML::image('img/csc-logo.png', null, ['class' => 'logo']) }}
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Mission</h1>
+            <p>Gawing Lingkod-Bayani ang Bawat Kawani</p>
+        </div>
+        <div class="col-md-6">
+            <h1>Vision</h1>
+            <p>CSC shall be Asia's leading center of excellence for strategic Human Resource (HR) and Organizational Development (OD)</p>
+        </div>
+    </div>
+</div>
 @stop
