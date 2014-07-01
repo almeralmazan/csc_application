fdf151d60003bf1c16a11e6d73e984c668d1177b
Almer Almazan <almeralmazan@gmail.com>
2014-07-01 22:28:31 +0800

    Add route filter for admin

--- app/filters.php
+++ app/filters.php
@@ -32,6 +32,13 @@ App::after(function($request, $response)
 | integrates HTTP Basic authentication for quick, simple checking.
 |
 */
+Route::filter('admin', function()
+{
+    if (Auth::user()->role != 'admin')
+    {
+        return Redirect::to('admin/login');
+    }
+});
 
 Route::filter('auth', function()
 {
