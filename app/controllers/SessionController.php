67ce87090c60665d0422f1d7bd2a1a63cf262082
Almer Almazan <almeralmazan@gmail.com>
2014-07-01 22:43:27 +0800

    Add implementation methods

---
+++ app/controllers/SessionController.php
@@ -0,0 +1,24 @@
+<?php
+
+class SessionController extends BaseController {
+
+    public function processorLogin()
+    {
+
+    }
+
+    public function processorLogout()
+    {
+
+    }
+
+    public function adminLogin()
+    {
+
+    }
+
+    public function adminLogout()
+    {
+
+    }
+}
