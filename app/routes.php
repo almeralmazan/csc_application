ba634d401d4b61679db5ed877908a83d52f5cb5a
Almer Almazan <almeralmazan@gmail.com>
2014-07-01 22:30:44 +0800

    Add admin routes

--- app/routes.php
+++ app/routes.php
@@ -5,3 +5,14 @@ Route::get('/', 'HomeController@home');
 Route::get('application-form', 'HomeController@applicationForm');
 Route::get('list-of-passers', 'HomeController@listOfPassers');
 Route::get('payment', 'HomeController@payment');
+
+Route::get('admin/login', 'AdminController@loginPage');
+
+// Processor Pages
+
+
+
+// Admin Pages
+Route::group(['before' => 'auth|admin', 'prefix' => 'admin'], function() {
+});
+
