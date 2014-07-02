749be525b28e0756b86159f5906ea3568e9fd179
Almer Almazan <almeralmazan@gmail.com>
2014-07-02 11:39:03 +0800

    Add methods in AdminController

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -2,9 +2,39 @@
 
 class AdminController extends BaseController {
 
+    public function index()
+    {
+        $title = 'Admin Page';
+        return View::make('admin.index', compact('title'));
+    }
+
     public function loginPage()
     {
         $title = 'Admin Login Page';
         return View::make('admin.login-page', compact('title'));
+    }
+
+    public function verifyLogin()
+    {
+        $credentials = Auth::attempt([
+            'username' => Input::get('username'),
+            'password' => Input::get('password')
+        ]);
+
+        if ( ! $credentials)
+        {
+            return Response::json([
+                'success' => false,
+                'message' => 'Invalid user/password input'
+            ]);
+        }
+
+        return Response::json(['success' => true]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return Redirect::to('admin/login');
     }
 }
