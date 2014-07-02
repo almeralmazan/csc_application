c8fd3d92b0b07548a95946b6cb7b1441d4152b15
Almer Almazan <almeralmazan@gmail.com>
2014-07-02 14:36:59 +0800

    Add methods to ProcessorController.php

--- app/controllers/ProcessorController.php
+++ app/controllers/ProcessorController.php
@@ -2,5 +2,39 @@
 
 class ProcessorController extends BaseController {
 
+    public function index()
+    {
+        $title = 'Processor Page';
+        return View::make('processor.index', compact('title'));
+    }
 
+    public function loginPage()
+    {
+        $title = 'Processor Login Page';
+        return View::make('processor.login-page', compact('title'));
+    }
+
+    public function verifyLogin()
+    {
+        $credentials = Auth::attempt([
+            'username' => Input::get('username'),
+            'password' => Input::get('password')
+        ]);
+
+        if ( ! $credentials)
+        {
+            return Response::json([
+                'success' => false,
+                'message' => 'Invalid user/password input'
+            ]);
+        }
+
+        return Response::json(['success' => true]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return Redirect::to('processor/login');
+    }
 }
