74771987090f14146610511cff8cad80dd93939c
Almer Almazan <almeralmazan@gmail.com>
2014-07-02 15:04:58 +0800

    Add TestingCenterTableSeeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder {
         $this->call('CityProvinceTableSeeder');
         $this->command->info('CityProvince table seeded!');
 
-        $this->call('CscRegionalOfficeTableSeeder');
-        $this->command->info('CscRegionalOffice table seeded!');
+        $this->call('TestingCenterTableSeeder');
+        $this->command->info('TestingCenter table seeded!');
 
         $this->call('UserTableSeeder');
         $this->command->info('User table seeded!');
