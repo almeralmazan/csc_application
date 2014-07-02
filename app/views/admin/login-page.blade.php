c860c5264913fb827235224fd279ef74c21f189a
Almer Almazan <almeralmazan@gmail.com>
2014-07-02 11:45:26 +0800

    Replace form and inputs with Form Laravel class

--- app/views/admin/login-page.blade.php
+++ app/views/admin/login-page.blade.php
@@ -4,16 +4,19 @@
 <div id="fullscreen_bg" class="fullscreen_bg" />
 
 <div class="container container-login">
-    <form class="form-signin">
-        <h1 class="form-signin-heading text-muted">Admin Login</h1>
-        <input type="text" class="form-control" placeholder="Username" required="" autofocus="">
-        <input type="password" class="form-control" placeholder="Password" required="">
+    {{ Form::open(['url' => 'admin/login', 'class' => 'form-signin', 'id' => 'admin-form']) }}
+        <h1 class="form-signin-heading text-muted">
+            Admin Login
+        </h1>
+
+        {{ Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Username', 'autofocus', 'required']) }}
+        {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password', 'required']) }}
+
         <button class="btn btn-lg btn-primary btn-block" type="submit">
             Sign In
         </button>
-        <div class="alert alert-danger login-error" role="alert">
-            Error login
-        </div>
-    </form>
+
+        <div class="login-error"></div>
+    {{ Form::close() }}
 </div>
 @stop
