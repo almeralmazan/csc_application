69a72392980b77359fe9f7f6e6905b5394cf0470
Almer Almazan <almeralmazan@gmail.com>
2014-07-11 10:25:37 +0800

    Fix indentation in Schedule model class getAllSchedules()

--- app/models/Schedule.php
+++ app/models/Schedule.php
@@ -7,17 +7,17 @@ class Schedule extends Eloquent {
     public static function getAllSchedules()
     {
         $result = DB::table('schedules')
-            ->join('testing_centers', 'testing_centers.id', '=', 'schedules.testing_center_id')
-            ->select(
-                'testing_centers.location',
-                'schedules.id',
-                'schedules.testing_center_id',
-                'schedules.date_start',
-                'schedules.time_start',
-                'schedules.time_end'
-            )
-            ->orderBy('testing_centers.location')
-            ->get();
+                    ->join('testing_centers', 'testing_centers.id', '=', 'schedules.testing_center_id')
+                    ->select(
+                        'testing_centers.location',
+                        'schedules.id',
+                        'schedules.testing_center_id',
+                        'schedules.date_start',
+                        'schedules.time_start',
+                        'schedules.time_end'
+                    )
+                    ->orderBy('testing_centers.location')
+                    ->get();
 
         return $result;
     }
