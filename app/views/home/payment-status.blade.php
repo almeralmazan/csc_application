bc083e6609926533b375e359aadbbaf0229dcaf0
Almer Almazan <almeralmazan@gmail.com>
2014-07-28 18:58:15 +0800

    add button in list of passers

--- app/views/home/payment-status.blade.php
+++ app/views/home/payment-status.blade.php
@@ -8,15 +8,31 @@
 <div class="container content">
     <div class="row margin-top">
         <div class="col-md-4 col-md-offset-4">
-            <input id="control-number-field" class="form-control" placeholder="Type your control # here" name="control_number_field" type="text">
+            <div class="input-group">
+                <input id="control-number-field" class="form-control" placeholder="Type your control # here" name="control_number_field" type="text">
+
+                <span class="input-group-btn">
+                    <button type="button" class="button btn btn-default" name="subscribe" id="search-applicant-control-number">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+            </div>
         </div><!-- /.col-md-6 -->
     </div>
     <div class="row margin-top">
         <div class="col-md-4 col-md-offset-4" id="status-container">
             <div class="well">
                 <div class="row">
+                    <div class="col-md-5">
+                        <h3><small>Exam Status:</small></h3>
+                    </div>
+                    <div class="col-md-7">
+                        <h3><span class="label label-success" id="exam-status"></span></h3>
+                    </div>
+                </div>
+                <div class="row">
                     <div class="col-md-4">
-                        <h3><small>Status:</small></h3>
+                        <h3><small>Paid Status:</small></h3>
                     </div>
                     <div class="col-md-8">
                         <h3><span class="label label-success" id="status-paid"></span></h3>
