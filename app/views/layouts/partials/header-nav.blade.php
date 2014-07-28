a7e06e691719024500dce6342ba92c53ec7b7b45
Almer Almazan <almeralmazan@gmail.com>
2014-07-28 18:58:45 +0800

    Reorder nav links in home

--- app/views/layouts/partials/header-nav.blade.php
+++ app/views/layouts/partials/header-nav.blade.php
@@ -7,11 +7,11 @@
             <a href="/application-form" class="list-group-item {{ set_active('application-form') }}">
                 <i class="fa fa-search"></i><span class="glyphicon glyphicon-pencil"></span>  Application
             </a>
-            <a href="/list-of-passers" class="list-group-item {{ set_active('list-of-passers') }}">
-                <i class="fa fa-user"></i><span class="glyphicon glyphicon-list-alt"></span>  List of passers
-            </a>
             <a href="/payment-status" class="list-group-item {{ set_active('payment-status') }}">
-                <i class="fa fa-folder-open-o"></i><span class="glyphicon glyphicon-usd"></span> Payment
+                <i class="fa fa-folder-open-o"></i><span class="glyphicon glyphicon-list-alt"></span> List Of Passers
+            </a>
+            <a href="/list-of-passers" class="list-group-item {{ set_active('list-of-passers') }}">
+                <i class="fa fa-user"></i><span class="glyphicon glyphicon-usd"></span> Payment
             </a>
         </div>
     </div>
