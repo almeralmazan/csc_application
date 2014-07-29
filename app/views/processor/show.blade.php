4222d3f184ac35fcd70fc0a0779675e6d86cd0b9
Almer Almazan <almeralmazan@gmail.com>
2014-07-29 09:07:10 +0800

    Remove .row and other col-md and col-xs

--- app/views/processor/show.blade.php
+++ app/views/processor/show.blade.php
@@ -1,13 +1,7 @@
 @extends('layouts.admin-processor')
 
 @section('content')
-<div class="row">
-    <div class="col-md-1">
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-10 col-lg-8 col-xs-offset-0 col-sm-offset-0 col-md-offset-1 col-lg-offset-2 margin-top" >
+    <div class="col-md-8 margin-top" >
         <div class="panel panel-info">
             <div class="panel-heading">
                 <h3 class="panel-title">{{ $applicant->applicant_first_name .' '. $applicant->applicant_last_name }}</h3>
@@ -105,7 +99,6 @@
 
     </div>
 
-    </div>
 </div>
 
 @stop
