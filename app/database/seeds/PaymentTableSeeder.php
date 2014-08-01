857fab58510d6c59aa876dfaabc5bc4fe7d55f32
Almer Almazan <almeralmazan@gmail.com>
2014-08-01 14:01:09 +0800

    Add reserved_date column in PaymentTableSeeder.php

--- app/database/seeds/PaymentTableSeeder.php
+++ app/database/seeds/PaymentTableSeeder.php
@@ -8,30 +8,35 @@ class PaymentTableSeeder extends Seeder {
 
         Payment::create([
             'applicant_id'  =>  1,
+            'reserved_date' =>  '2014-07-03',
             'paid_date'     =>  '2014-07-05',
             'price'         =>  500.00
         ]);
 
         Payment::create([
             'applicant_id'  =>  2,
+            'reserved_date' =>  '2014-07-03',
             'paid_date'     =>  '2014-07-03',
             'price'         =>  500.00
         ]);
 
         Payment::create([
             'applicant_id'  =>  3,
+            'reserved_date' =>  '2014-07-13',
             'paid_date'     =>  '0000-00-00',
             'price'         =>  500.00
         ]);
 
         Payment::create([
             'applicant_id'  =>  4,
+            'reserved_date' =>  '2014-07-20',
             'paid_date'     =>  '2014-07-22',
             'price'         =>  500.00
         ]);
 
         Payment::create([
             'applicant_id'  =>  5,
+            'reserved_date' =>  '2014-07-25',
             'paid_date'     =>  '0000-00-00',
             'price'         =>  500.00
         ]);
