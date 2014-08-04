a1fb5fc7aa952ae90bad81309a874b86506eb4b0
Almer Almazan <almeralmazan@gmail.com>
2014-08-04 18:26:54 +0800

    Fix accordion

--- app/views/layouts/partials/header-nav.blade.php
+++ app/views/layouts/partials/header-nav.blade.php
@@ -4,12 +4,15 @@
         <div class="panel panel-default">
             <div class="panel-heading">
                 <h4 class="panel-title">
-                    <a class="{{ set_active('/') }}" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-                        Home
+<!--                    <a class="{{ set_active('/') }}" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">-->
+<!--                        Home-->
+<!--                    </a>-->
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="{{ set_active('/') }} center-block">
+                        Home <span class="caret" style="margin-left: 70px;"></span>
                     </a>
                 </h4>
             </div>
-            <div id="collapseOne" class="panel-collapse collapse in">
+            <div id="collapseOne" class="panel-collapse collapse">
                 <div class="panel-body">
                     <ul class="list-unstyled">
                         <li><a href="/" class="{{ set_active_accordion_link('/') }}">News</a></li>
