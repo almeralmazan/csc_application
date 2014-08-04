de37c67762329609869c3313fe52718604ddd98e
Almer Almazan <almeralmazan@gmail.com>
2014-08-04 23:32:08 +0800

    Add exam status results

--- app/views/home/list-of-passers.blade.php
+++ app/views/home/list-of-passers.blade.php
@@ -28,6 +28,16 @@
                 <div class="row">
                     <div class="col-md-4">
                         <h3>
+                            <small>Exam Status:</small>
+                        </h3>
+                    </div>
+                    <div class="col-md-8">
+                        <h3><span class="label label-success" id="exam-status"></span></h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <h3>
                             <small>Paid Status:</small>
                         </h3>
                     </div>
