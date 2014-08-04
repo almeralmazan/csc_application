6857085eae0cf9ab5e9c06f1d7aabce010653098
Almer Almazan <almeralmazan@gmail.com>
2014-08-05 00:04:30 +0800

    Add Paid and Reserved functionality, nav links.

---
+++ app/views/admin/reserved.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.admin-processor')
+
+@section('content')
+<div class="col-md-10">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>List of Reserved Applicants</h2>
+            <hr>
+        </div>
+    </div>
+    <div class="row search-query">
+        <div class="col-md-3">
+            <input type="text" class="form-control" placeholder="Search name here" ng-model="search_name">
+        </div>
+        <!-- /.col-md-4 -->
+        <div class="col-md-3">
+            <select class="form-control" ng-model="exam_level">
+                <option value="Sub Pro">CSE - Sub Professional</option>
+                <option value="Professional">CSE - Professional</option>
+            </select>
+        </div>
+        <!-- /.col-md-4 -->
+    </div>
+    <div class="row table-container" ng-controller="ProcessorReservedController">
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th>Control number</th>
+                <th>Name</th>
+                <th>Schedule</th>
+                <th>Exam Level</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr ng-repeat="applicant in applicants | filter:exam_level | filter:search_name">
+                <td>{[ applicant.controlno ]}</td>
+                <td>
+                    <a href="{[ window.location.origin + '/processor/applicant/' + applicant.id ]}">
+                        {[ applicant.applicant_first_name + ' ' + applicant.applicant_last_name ]}
+                    </a>
+                </td>
+                <td>{[ applicant.schedule_date_start | date ]}</td>
+                <td>{[ applicant.new_exam_level ]}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+@stop
+
