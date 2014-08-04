3f8218c94962c440c618ac03a2c780a1c65bc742
Almer Almazan <almeralmazan@gmail.com>
2014-08-04 18:55:21 +0800

    Remove unnecessary tags

--- app/views/paypal/payment.blade.php
+++ app/views/paypal/payment.blade.php
@@ -18,12 +18,12 @@
                 <hr>
                 <div class="form-group">
                     <div class="col-sm-12">
-                        <input type="text" class="form-control" name="card-holder-name" id="card-holder-name" placeholder="First name" style="border-radius: 5px 0 0 0; width:183px; float:left">
-                        <input type="text" class="form-control" name="card-holder-name" id="card-holder-name" placeholder="Last name" style="border-radius: 0 5px 0 0; width:183px; float:left">
-                        <input type="text" class="form-control" name="card-holder-name" id="card-holder-name" placeholder="Card Number" style="border-radius: 0;">
-                        <input type="text" class="form-control" name="card-holder-name" id="card-holder-name" placeholder="MM" style="border-radius: 0px 0 0 5px; width:143px; float:left">
-                        <input type="text" class="form-control" name="card-holder-name" id="card-holder-name" placeholder="YYYY" style="border-radius: 0; width:143px; float:left">
-                        <input type="text" class="form-control" name="card-holder-name" id="card-holder-name" placeholder="C V V" style="border-radius: 0 0px 5px 0; width:80px; float:left">
+                        <input type="text" class="form-control" name="firstName" placeholder="First name" style="border-radius: 5px 0 0 0; width:183px; float:left">
+                        <input type="text" class="form-control" name="lastName" placeholder="Last name" style="border-radius: 0 5px 0 0; width:183px; float:left">
+                        <input type="text" class="form-control" name="number" placeholder="Card Number" style="border-radius: 0;">
+                        <input type="text" class="form-control" name="expiryMonth" placeholder="MM" style="border-radius: 0px 0 0 5px; width:143px; float:left">
+                        <input type="text" class="form-control" name="expiryYear" placeholder="YYYY" style="border-radius: 0; width:143px; float:left">
+                        <input type="text" class="form-control" name="cvv" placeholder="C V V" style="border-radius: 0 0px 5px 0; width:80px; float:left">
                     </div>
                     <div class="col-sm-12 margin-top text-center">
                         <div class="row">
